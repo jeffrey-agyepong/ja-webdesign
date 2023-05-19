68b7fe1bc176447fb5fa9f7c0aa921dcd614e6cd
Jeffrey A <agyepong99@gmail.com>
2023-05-19 13:49:06 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,91 +1,79 @@
-import Swup from 'swup';
-import SwupScrollPlugin from '@swup/scroll-plugin';
-
-document.addEventListener("DOMContentLoaded", function () {
+// import Swup from 'swup';
+// import SwupScrollPlugin from '@swup/scroll-plugin';
 
 
 
+function closeMobileNav() {
+    var nav = document.getElementById("mobile-nav");
+    nav.classList.toggle("hidden");
+}
 
-    function closeMobileNav() {
-        var nav = document.getElementById("mobile-nav");
-        nav.classList.toggle("hidden");
-    }
+const options = {
+    animationSelector: '[class*="transition-fade"]',
+    animateHistoryBrowsing: true,
+    plugins: [
 
-    const options = {
-        animationSelector: '[class*="transition-fade"]',
-        animateHistoryBrowsing: true,
-        plugins: [
+        new SwupScrollPlugin({
+            animateScroll: false
+        })
+    ]
+};
+const swup = new Swup(options);
+// const swup = new Swup(options);
 
-            new SwupScrollPlugin({
-                animateScroll: false
-            })
-        ]
-    };
-    const swup = new Swup(options);
-    // const swup = new Swup(options);
+let scrollValues = {};
 
-    let scrollValues = {};
+swup.on('clickLink', () => {
+    scrollValues[window.location.href] = window.scrollY;
+});
 
-    swup.on('clickLink', () => {
-        scrollValues[window.location.href] = window.scrollY;
-    });
+swup.on('popState', () => {
+    setTimeout(function () {
+        window.scrollTo(0, scrollValues[window.location.href]);
+    }, 100);
+});
+var themeToggleDarkIcon = document.getElementById('theme-toggle-dark-icon');
+var themeToggleLightIcon = document.getElementById('theme-toggle-light-icon');
 
-    swup.on('popState', () => {
-        setTimeout(function () {
-            window.scrollTo(0, scrollValues[window.location.href]);
-        }, 100);
-    });
-    var themeToggleDarkIcon = document.getElementById('theme-toggle-dark-icon');
-    var themeToggleLightIcon = document.getElementById('theme-toggle-light-icon');
+// Change the icons inside the button based on previous settings
+if (localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+    themeToggleLightIcon.classList.remove('hidden');
+} else {
+    themeToggleDarkIcon.classList.remove('hidden');
+}
 
-    // Change the icons inside the button based on previous settings
-    if (localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-        themeToggleLightIcon.classList.remove('hidden');
-    } else {
-        themeToggleDarkIcon.classList.remove('hidden');
-    }
+var themeToggleBtn = document.getElementById('theme-toggle');
 
-    var themeToggleBtn = document.getElementById('theme-toggle');
+themeToggleBtn.addEventListener('click', function () {
 
-    themeToggleBtn.addEventListener('click', function () {
+    // toggle icons inside button
+    themeToggleDarkIcon.classList.toggle('hidden');
+    themeToggleLightIcon.classList.toggle('hidden');
 
-        // toggle icons inside button
-        themeToggleDarkIcon.classList.toggle('hidden');
-        themeToggleLightIcon.classList.toggle('hidden');
-
-        // if set via local storage previously
-        if (localStorage.getItem('color-theme')) {
-            if (localStorage.getItem('color-theme') === 'light') {
-                document.documentElement.classList.add('dark');
-                localStorage.setItem('color-theme', 'dark');
-            } else {
-                document.documentElement.classList.remove('dark');
-                localStorage.setItem('color-theme', 'light');
-            }
-
-            // if NOT set via local storage previously
+    // if set via local storage previously
+    if (localStorage.getItem('color-theme')) {
+        if (localStorage.getItem('color-theme') === 'light') {
+            document.documentElement.classList.add('dark');
+            localStorage.setItem('color-theme', 'dark');
         } else {
-            if (document.documentElement.classList.contains('dark')) {
-                document.documentElement.classList.remove('dark');
-                localStorage.setItem('color-theme', 'light');
-            } else {
-                document.documentElement.classList.add('dark');
-                localStorage.setItem('color-theme', 'dark');
-            }
+            document.documentElement.classList.remove('dark');
+            localStorage.setItem('color-theme', 'light');
         }
 
-    });
-
-
-    // // ===== Faq accordion
-    // const faqs = document.querySelectorAll(".single-faq");
-    // faqs.forEach((el) => {
-    //     el.querySelector(".faq-btn").addEventListener("click", () => {
-    //         el.querySelector(".icon").classList.toggle("rotate-180");
-    //         el.querySelector(".faq-content").classList.toggle("hidden");
-    //     });
-    // });
-
+        // if NOT set via local storage previously
+    } else {
+        if (document.documentElement.classList.contains('dark')) {
+            document.documentElement.classList.remove('dark');
+            localStorage.setItem('color-theme', 'light');
+        } else {
+            document.documentElement.classList.add('dark');
+            localStorage.setItem('color-theme', 'dark');
+        }
+    }
 
 });
+
+
+
+
 

f4f79051fd2bdfaf622e3c8bf2123a5927497c5d
Jeffrey A <agyepong99@gmail.com>
2023-05-21 01:30:24 -0600

    Update vite.config.cjs

--- vite.config.cjs
+++ vite.config.cjs
@@ -7,6 +7,9 @@ module.exports = {
                 main: 'index.html',
                 about: 'about.html',
                 contact: 'contact.html',
+                pricing: 'pricing.html',
+                services: 'services.html',
+                projects: 'all-projects.html',
                 privacyPolicy: 'privacy-policy.html',
 
             }
